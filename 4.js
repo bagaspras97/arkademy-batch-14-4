56b41f9769e086c31d4e09a6e8c3090183caeaee
bagaspras97 <59312912+bagaspras97@users.noreply.github.com>
2020-01-18 15:45:56 +0700

    Add files via upload

---
+++ 4.js
@@ -0,0 +1,27 @@
+function cetak_gambar(number){
+  var reg = /[0-9]/;
+  var dua = 2;
+  var satu = 1;
+  if (reg.test(number) && number%dua === 1){
+    var panjang = number;
+      console.log("--- panjang ---")
+     for(i=1; i<=10; i++){
+       for(dua; i<=dua; i++){
+         console.log("*  =   =   =  *")
+//          console.log(i)
+       }for(satu; satu+2>=i; i++){
+        console.log("*  *   *   *  *");
+//         console.log(i);
+       }for(dua; dua+3>=i; i++){
+         console.log("*  =   =   =  *")
+//          console.log(i)
+       }
+        console.log("")
+     }
+  }else{
+    console.log("parameter harus angka ganjil")
+  }
+ 
+}
+
+cetak_gambar(5);
